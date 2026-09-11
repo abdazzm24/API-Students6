c61e3ddc4308e51902718994e152c31f422e2db0
Abdullah Azzam <abdazzm24@gmail.com>
2026-09-11 22:25:15 +0700

    tambah auth middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -2,38 +2,57 @@ package middleware
 
 import (
 	"log/slog"
+	"strings"
 	"time"
 
 	"api-students/helper"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/helmet"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/gofiber/fiber/v2/middleware/requestid"
 )
-
-func RequireJSON(c *fiber.Ctx) error {
-	if !c.Is("json") {
-		return helper.Fail(
-			c,
-			fiber.StatusUnsupportedMediaType,
-			"Content-Type harus application/json",
-			nil,
-		)
-	}
-
-	return c.Next()
-}
 
 func Register(
 	app *fiber.App,
 	logger *slog.Logger,
+	allowedOrigins string,
 ) {
 
 	app.Use(requestid.New())
 
-	app.Use(cors.New())
+	app.Use(recover.New())
+
+	app.Use(helmet.New())
+
+	app.Use(corsPolicy(allowedOrigins))
 
 	app.Use(RequestLogger(logger))
+}
+
+func corsPolicy(
+	allowedOrigins string,
+) fiber.Handler {
+
+	if strings.TrimSpace(
+		allowedOrigins,
+	) == "" {
+		allowedOrigins =
+			"http://localhost:5173"
+	}
+
+	return cors.New(
+		cors.Config{
+			AllowOrigins: allowedOrigins,
+
+			AllowMethods:
+				"GET,POST,PUT,PATCH,DELETE,OPTIONS",
+
+			AllowHeaders:
+				"Origin,Content-Type,Accept,Authorization",
+		},
+	)
 }
 
 func RequestLogger(
@@ -46,12 +65,15 @@ func RequestLogger(
 
 		err := c.Next()
 
+		requestID, _ :=
+			c.Locals("requestid").(string)
+
 		logger.Info(
 			"http_request",
 
 			slog.String(
 				"request_id",
-				c.Get("X-Request-ID"),
+				requestID,
 			),
 
 			slog.String(
@@ -73,8 +95,46 @@ func RequestLogger(
 				"duration",
 				time.Since(start),
 			),
+
+			slog.String(
+				"ip",
+				c.IP(),
+			),
 		)
 
 		return err
 	}
+}
+
+var methodsWithBody = map[string]bool{
+	fiber.MethodPost:  true,
+	fiber.MethodPut:   true,
+	fiber.MethodPatch: true,
+}
+
+func RequireJSON(
+	c *fiber.Ctx,
+) error {
+
+	if methodsWithBody[c.Method()] {
+
+		ct := c.Get(
+			"Content-Type",
+		)
+
+		if !strings.HasPrefix(
+			ct,
+			fiber.MIMEApplicationJSON,
+		) {
+
+			return helper.Fail(
+				c,
+				fiber.StatusUnsupportedMediaType,
+				"Content-Type harus application/json",
+				nil,
+			)
+		}
+	}
+
+	return c.Next()
 }
